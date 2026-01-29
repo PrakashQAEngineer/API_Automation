2daec8805d56b3243813e92b05c17d14eb106980
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2026-01-29 09:06:11 +0530

    Add files via upload

---
+++ RestAssured_Wedding/src/Payload/updateAllMethodin1.java
@@ -0,0 +1,17 @@
+package Payload;
+
+import RestAssuredMainData.AllMethodin1;
+
+public class updateAllMethodin1 
+{
+	
+	public static String updateAllMethodin11()
+	{
+		return "{\r\n"
+				+ "    \"place_id\": \""+AllMethodin1.place+"\",\r\n"
+				+ "    \"address\": \"Rajinder t\",\r\n"
+				+ "    \"key\": \"qaclick123\"\r\n"
+				+ "}";
+	}
+
+}
