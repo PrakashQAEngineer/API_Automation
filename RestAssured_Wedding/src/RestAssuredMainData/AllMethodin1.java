c0b220d4cfe1fe2eb610bf04f5b636577ede9360
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2026-01-29 09:05:33 +0530

    Add files via upload

---
+++ RestAssured_Wedding/src/RestAssuredMainData/AllMethodin1.java
@@ -0,0 +1,39 @@
+package RestAssuredMainData;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import Payload.AllMethodin1BodyData;
+import Payload.updateAllMethodin1;
+
+public class AllMethodin1 
+{
+	public static String  place = ""; 
+	
+	public static void main(String args[])
+	{
+		
+		
+		//add place
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+	String res = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+		.body(AllMethodin1BodyData.getBodyAllMethodin1()).when().post("maps/api/place/add/json")
+		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"), "status",equalTo("OK")).extract().response().asString();
+	
+	  JsonPath js = new JsonPath(res);
+	   place = js.getString("place_id");
+	   System.out.println(place);
+	   
+	   //update place
+	   System.out.println("********************** Update Place ************************");
+	   given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+	   .body(updateAllMethodin1.updateAllMethodin11()).when().put("maps/api/place/update/json").then().log().all()
+	   .assertThat().statusCode(200);
+	   
+	   
+	}
+
+}
