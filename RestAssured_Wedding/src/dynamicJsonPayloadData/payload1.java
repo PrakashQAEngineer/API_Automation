20380c838c7a774a69ba0972247ac063f69f1e8b
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2026-02-17 09:03:32 +0530

    Refactor payDynamic method to accept parameters

--- RestAssured_Wedding/src/dynamicJsonPayloadData/payload1.java
+++ RestAssured_Wedding/src/dynamicJsonPayloadData/payload1.java
@@ -2,12 +2,12 @@ package dynamicJsonPayloadData;
 
 public class payload1 
 {
-	 public static String payDynamic()
+	 public static String payDynamic(String isbn,String aisle)
 	 {
 		 return "{\r\n"
 		 		+ "\"name\":\"Learn Selenium Automation with Java\",\r\n"
-		 		+ "\"isbn\":\"bce\",\r\n"
-		 		+ "\"aisle\":\"2978927\",\r\n"
+		 		+ "\"isbn\":\""+isbn+"\",\r\n"
+		 		+ "\"aisle\":\""+aisle+"\",\r\n"
 		 		+ "\"author\":\"Prakash Singh\"\r\n"
 		 		+ "}\r\n"
 		 		+ "";
