58417f4cc3769441033ac44911bc9352cc96dd76
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2026-02-17 09:04:33 +0530

    Update DynaJson to use DataProvider for addBook
    Refactored addBook method to use DataProvider for dynamic input.

--- RestAssured_Wedding/src/DynamicJson/DynaJson.java
+++ RestAssured_Wedding/src/DynamicJson/DynaJson.java
@@ -1,5 +1,6 @@
 package DynamicJson;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import dynamicJsonPayloadData.payload1;
@@ -10,12 +11,12 @@ import static io.restassured.RestAssured.*;
 
 public class DynaJson 
 {
-	@Test
+	@Test(dataProvider = "BooksData")
 	
-	public void addBook()
+	public void addBook(String isbn, String aisle)
 	{
 	   RestAssured.baseURI = "https://rahulshettyacademy.com";
-	   String res = given().log().all().header("Content-Type", "application/json").body(payload1.payDynamic())
+	   String res = given().log().all().header("Content-Type", "application/json").body(payload1.payDynamic(isbn,aisle))
 	   .when().post("Library/Addbook.php").then().log().all().assertThat().statusCode(200).extract().response().asString();
 	   
 	   JsonPath js = new JsonPath(res);
@@ -23,5 +24,13 @@ public class DynaJson
 	     System.out.println(id);
 	   
 	}
+	
+	 @DataProvider(name = "BooksData")
+	 
+	 public Object[][] getData()
+	 {
+		 return new Object[][]   {{"prak","9881"}, {"Jyoti", "8877"}, {"SparshYog", "9980"}};
+	 }
+	
 
 }
